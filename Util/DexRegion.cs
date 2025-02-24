db6684aef3208a40f137bcd74baf024aeb7a34c4
Zerker-TF <84744670+Zerker-TF@users.noreply.github.com>
2025-02-24 17:05:44 -0300

    Add files via upload

---
+++ Util/DexRegion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PokDex.Util
+{
+    public class DexRegion
+    {
+        public class Region
+        {
+            public string Name { get; set; } = "";
+            public int StartId { get; set; }
+            public int EndId { get; set; }
+        }
+
+        public static readonly Dictionary<string, Region> Regiones = new Dictionary<string, Region>
+        {
+            {"all", new Region{Name = "all",StartId=1,EndId=1025 } },
+            {"Kanto", new Region {Name="Kanto",StartId=1,EndId=151 } },
+            {"Johto", new Region {Name="Johto",StartId=152,EndId=251} },
+            {"Hoenn", new Region {Name="Hoenn",StartId=252,EndId=386} },
+            {"Sinnoh", new Region {Name="Sinnoh",StartId=387,EndId=493} },
+            {"Unova", new Region {Name="Unova",StartId=494,EndId=649} },
+            {"Kalos", new Region {Name="Kalos",StartId=650,EndId=721} },
+            {"Alola", new Region {Name="Alola",StartId=722,EndId=809} },
+            {"Galar", new Region {Name="Galar",StartId=810,EndId=905} },
+            {"Paldea", new Region {Name="Paldea",StartId=906,EndId=1025} }
+        };
+
+        public static List<int> FilterRegion(string regionName, List<int> allPokemonId)
+        {
+            if (Regiones.TryGetValue(regionName, out Region? region))
+            {
+                return allPokemonId.Where(id => id >= region.StartId && id <= region.EndId).ToList();
+            }
+
+            return new List<int>();
+        }
+    }
+}
---
+++ Util/PokeStatName.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace PokDex.Util
+{
+    public static class PokeStatName
+    {
+        // Creamos un diccionario con los nombres para los stats
+        private static readonly Dictionary<string, string> StatNameMap = new Dictionary<string, string>
+        {
+            { "hp", "HP" },
+            { "attack", "ATK" },
+            { "defense", "DEF" },
+            { "special-attack", "S.ATK" },
+            { "special-defense", "S.DEF" },
+            { "speed", "SPD" }
+        };
+
+        // metodo para que haga uso del diccionario y devuelva el nombre corto
+        public static string GetShortName(string orgName)
+        {
+            return StatNameMap.TryGetValue(orgName, out var shortName) ? shortName : orgName;
+        }
+    }
+}
